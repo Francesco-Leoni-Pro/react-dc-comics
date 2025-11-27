72ffea0d4855f1cd4d5b1dd9e86b90c8beba6c46
Francesco-Leoni-Pro <francesco.leoni.pro@icloud.com>
2025-11-27 03:13:07 +0100

    feat: fumetti dinamici

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,17 +1,31 @@
-import style from "./AppMain.module.css"
-import LogoStrip from "./LogoStrings"
+import style from "./AppMain.module.css";
+import LogoStrip from "./LogoStrings";
+import Jumbotron from "../assets/img/jumbotron.jpg";
+import comics from "../assets/comics";
 
 export default function AppMain() {
   return (
-    <main className={`${style.main} ${style["bg-black"]} ${style["text-white"]}`}>
+    <main
+      className={`${style.main} ${style["bg-black"]} ${style["text-white"]}`}
+      style={{ backgroundImage: `url(${Jumbotron})` }}
+    >
       <div className={style.section}>
         <div className={style.textContainer}>
-          <h3 className={style.textLeft}>{"-->Element goes here<--"}</h3>
-        </div>
-        <div className={style.logoContainer}>
-          <LogoStrip />
+          <h3 className={style.textLeft}>CURRENT SERIES</h3>
         </div>
       </div>
+      <div className={style.comicsGrid}>
+        {comics.map((comic) => (
+          <div key={comic.id} className={style.comicCard}>
+            <img src={comic.thumb} alt={comic.title} />
+            <h4>{comic.title.toUpperCase()}</h4>
+            {/* Prezzo rimosso */}
+          </div>
+        ))}
+      </div>
+      <div className={style.logoContainer}>
+        <LogoStrip />
+      </div>
     </main>
-  )
+  );
 }
